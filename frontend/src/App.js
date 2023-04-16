f34d8bbb2aab73ac293f714602f569f449d5a241
farukh110 <farukhsajjad110@gmail.com>
2023-04-16 12:10:45 +0500

    initial project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Coming soon new website
+          <br />
+          Contact Number: 03414285511
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
